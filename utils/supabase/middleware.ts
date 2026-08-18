f9414540f80590a7e075cf037db906cf87d55281
doraelysiane <doraelysiane@example.com>
2026-08-18 21:59:34 +0000

    Allow unauthenticated access to landing page

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -34,6 +34,7 @@ export async function updateSession(request: NextRequest) {
   // Always protect routes under /(app)
   if (
     !user &&
+    request.nextUrl.pathname !== '/' &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/register') &&
     !request.nextUrl.pathname.startsWith('/auth')
