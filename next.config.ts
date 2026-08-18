f78741739e89083710750bc21c4519e7d3011de5
doraelysiane <doraelysiane@example.com>
2026-08-18 11:32:24 +0000

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    DATA_SOURCE: 'supabase',
+    NEXT_PUBLIC_SUPABASE_URL: 'https://zucxcvifjwmrfkforzzh.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'sb_publishable_Sk_1WZIu21WVN2qAUR77cg_LsLWbnYn',
+  },
 };
 
 export default nextConfig;
